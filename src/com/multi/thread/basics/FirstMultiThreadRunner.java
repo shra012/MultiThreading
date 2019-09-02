2b2d015c33ddc9d931631ff049d14a8895b78d00
shra012 <shravan_fisher@yahoo.ca>
2019-09-02 12:31:06 +0530

    Added basic comments

--- src/com/multi/thread/basics/FirstMultiThreadRunner.java
+++ src/com/multi/thread/basics/FirstMultiThreadRunner.java
@@ -19,7 +19,10 @@ public class FirstMultiThreadRunner {
 		runner.createThreadsUsingFolkJointPool(run);
 		System.out.println("The value in the Pojo is :"+multiThreadPojo.getValue());
 	}
-	
+	/**
+	 * This method is used to call the folk join pool for multi threading
+	 * @param run Runnable to be passed to do the multi-threading task
+	 */
 	public void createThreadsUsingFolkJointPool(Runnable run){
 		long start = System.currentTimeMillis();
 		Thread[] threads = new Thread[1_000];
@@ -47,7 +50,12 @@ public class FirstMultiThreadRunner {
 		long totalTimeTaken = end-start;
 		System.out.println("Total Time Elapsed in milli seconds for Folk Join Pool "+totalTimeTaken);
 	}
-
+	
+	/**
+	 * This method is used to create threads for multi-threading
+	 * @param run Runnable to be passed to do the multi-threading task
+	 * @throws InterruptedException
+	 */
 	public void manuallyCreateThreads(Runnable run) throws InterruptedException{
 		long start = System.currentTimeMillis();
 		Thread[] threads = new Thread[1_000];
